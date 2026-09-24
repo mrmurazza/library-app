7853ca089248a529135a0708ec09c23950d524dc
mrmurazza <user@users-MacBook-Pro.local>
2026-09-24 10:44:34 +0700

    add swagger config

---
+++ src/main/java/com/lexhive/libraryapp/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.lexhive.libraryapp.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI libraryOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Book Library API")
+                        .version("1.0")
+                        .description("Catalog, members, and loans. Borrowing limits and loan length come from configuration, not the database."))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new Components().addSecuritySchemes("basicAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("basic")));
+    }
+}
